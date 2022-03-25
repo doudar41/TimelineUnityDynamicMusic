27595b171a5416937111e625010563f30a1d7e28
doudar41 <60257081+doudar41@users.noreply.github.com>
2022-03-25 09:44:48 +0100

    Add files via upload

---
+++ MusicTimelineManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class MusicTimelineManager : MonoBehaviour
+{
+    private PlayableDirector director;
+    [SerializeField]
+    private Track[] tracks;
+
+
+    [SerializeField]
+    private int[] randomTracks;
+    [SerializeField]
+    private AudioSource transition;
+    private double timelineDuration;
+    private bool chooseRandomTrack = false;
+
+
+    public delegate void OnAttackingPlayer(int trackNumber,bool InOut, float fadeTime);
+    public static OnAttackingPlayer playerHunted;
+  
+
+
+
+    private void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    private void Start()
+    {
+        RandomTrackStart(randomTracks);
+        playerHunted += FadeInOutCertainTrack;
+        timelineDuration = director.duration;
+
+        StartCoroutine(DelayTimelineStart(0.1f));
+    }
+
+
+    private void Update()
+    {
+        if (director.time < 0.1f)
+        {
+            chooseRandomTrack = true;
+        }
+
+
+        if (director.time > timelineDuration - 0.1f && chooseRandomTrack)
+        {
+            chooseRandomTrack = false;
+            Debug.Log("Check");
+            FadeRandomTracksAtStart(randomTracks);
+            RandomTrackStart(randomTracks);
+        }
+    }
+
+    IEnumerator DelayTimelineStart(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        director.Play();
+    }
+
+    private void RandomTrackStart(int[] randomTrack)
+    {
+        foreach(int i in randomTrack)
+        {
+            int rand = UnityEngine.Random.Range(0, 3);
+            if (rand == 2)
+            {
+                tracks[i].FadeIn(0.3f);
+            }
+        }
+    }
+
+    private void FadeRandomTracksAtStart(int[] randomTrack)
+    {
+        foreach (int i in randomTrack)
+        {
+            tracks[i].FadeOut(0.01f);
+        }
+
+    }
+
+    private void FadeInOutCertainTrack(int trackNumber, bool InOut, float fadeTime)
+    {
+        if(InOut)tracks[trackNumber].FadeIn(fadeTime);
+        if(!InOut) tracks[trackNumber].FadeOut(fadeTime);
+    }
+
+    private void PlayTransitionSFX()
+    {
+
+    }
+
+    
+}
---
+++ Track.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*This script changes volume of attached AudioSource with different public functions*/
+
+[RequireComponent(typeof(AudioSource))] //Give us automaticaly AudioSource when attaching script to gameobject
+public class Track : MonoBehaviour
+{
+    private AudioSource track;
+    [SerializeField]
+    private float volumeCurrent = 0, volumeBase = 0;
+    [SerializeField]
+    private bool unmute = false;
+
+    private void Awake()
+    {
+        track = GetComponent<AudioSource>();
+        track.playOnAwake = false;
+    }
+
+    private void Start()
+    {
+        if (unmute)
+        {
+            FadeIn(volumeBase);
+        }
+    }
+
+    IEnumerator ChangeVolumeOvertime( float startVolume, float targetVolume, float duration)
+    {
+        float startingCoroutine = 0.0f;
+        while(startingCoroutine < duration)
+        {
+            volumeCurrent = Mathf.Lerp(startVolume, targetVolume, startingCoroutine / duration);
+            track.volume = volumeCurrent;
+            startingCoroutine += Time.deltaTime;
+            yield return null;
+        }
+
+        track.volume = volumeCurrent;
+    }
+
+    public void FadeIn(float fadeDuration)
+    {
+        StartCoroutine(ChangeVolumeOvertime(volumeCurrent, volumeBase, fadeDuration));
+    }    
+    
+    public void FadeOut(float fadeDuration)
+    {
+        StartCoroutine(ChangeVolumeOvertime(volumeCurrent, 0.0f, fadeDuration));
+    }
+
+    public void SetVolume(float vol)
+    {
+        track.volume = vol;
+    }
+
+}
